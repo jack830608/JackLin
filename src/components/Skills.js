b7e6a8c8d6a1c5d8dbc6cb0de416f208a1225b05
jack <jack@memepr.com>
2019-08-12 15:42:54 +0800

    update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,11 +13,12 @@ export default class Skills extends React.Component {
           <div className="SkillBox1">Font-End</div>
           <div className="SkillArrowLeft1">
             <li>HTML、CSS、JavaScript</li>
+            <li>SASS、SCSS</li>
             <li>jQuery</li>
             <li>RWD</li>
             <li>React、Redux</li>
-            <li>Next.js</li>
             <li>Styled Components</li>
+            <li>Next.js</li>
             <li>Angular.js (1.x)</li>
           </div>
           <div className="SkillArrowRight1"></div>
@@ -26,21 +27,20 @@ export default class Skills extends React.Component {
           <div className="SkillBox2">Back-End</div>
           <div className="SkillArrowLeft2">
             <li>Node.js</li>
-            <li>Express Framework</li>
+            <li>Express</li>
             <li>MongoDB、Mongoose</li>
             <li>RESTful API</li>
             <li>串接第三方 API</li>
-            <li>AWS</li>
+            <li>Nginx</li>
           </div>
           <div className="SkillArrowRight2"></div>
-
         </div>
         <div className="ArrowBox3">
           <div className="SkillBox3">Others</div>
           <div className="SkillArrowLeft3">
-            <li>Git、Gitlab、bitbucket</li>
-            <li>Nginx</li>
+            <li>AWS</li>
             <li>DigitalOcean</li>
+            <li>Git、Gitlab、bitbucket</li>
             <li>MVC design pattern</li>
             <li>Command Line</li>
             <li>Jira、Confluence</li>
