6d76ffda0fe01d5e3470dc8dda6eccb9894642ae
jack <jack@memepr.com>
2019-09-02 18:10:50 +0800

    update

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -80,7 +80,7 @@ export default (props) => {
                             </span>
                 <span className="timeline-content-info-date">
                   <i className="fa fa-calendar-o" aria-hidden="true"></i>
-                  2018 年 6 月 - 至今
+                  2018 年 6 月 -  2019 年 8 月
                             </span>
               </div>
               <p>因為對人工智慧的熱愛進入了MemePR，在這裡沒有職位大小區分，只要做的事是對的就可以無限發揮，在大家的努力下也成功打造出我們的第一隻聊天機器人
@@ -100,6 +100,36 @@ export default (props) => {
                 <li>Node.js</li>
                 <li>React、Redux、Styled Components</li>
                 <li>Next.js</li>
+              </ul>
+            </div>
+          </div>
+          <div className="cd-timeline-block" >
+            <div className="cd-timeline-img cd-picture">
+            </div>
+
+            <div className="cd-timeline-content">
+              <h2>Tripmoment時刻旅行</h2>
+              <div className="timeline-content-info">
+                <span className="timeline-content-info-title">
+                  <i className="fa fa-certificate" aria-hidden="true"></i>
+                  前端工程師
+                            </span>
+                <span className="timeline-content-info-date">
+                  <i className="fa fa-calendar-o" aria-hidden="true"></i>
+                  2019 年 8 月 -  至今
+                            </span>
+              </div>
+              <p>在時刻旅行這段時間負責的事更多了，前端後端都包，也讓我學到更扎實的技術，並且也持續幫助公司開發新產品。
+                  </p>
+              <ul className="content-skills">
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>JavaScript</li>
+                <li>React、Redux</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>ejs</li>
+                <li>PostgreSQL</li>
               </ul>
             </div>
           </div>
