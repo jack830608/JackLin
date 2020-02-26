5a7ff99a86e5595213859fe26458846ff1fafb6f
JackLin <jack830608@gmail.com>
2020-02-27 01:10:07 +0800

    update

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -15,11 +15,23 @@ export default (props) => {
             <p>My name is Jack Lin, have three years of experience in developing back-end and front-end.</p>
             <p>I am passionate about pursuing new knowledge, new skills and enjoying the sense of accomplishment of software development.</p>
             <p>For me , coding is to make life more convenient & solve problems in life, so I am happy to challenge new things and learn many new skills.</p>
+            <div className="location">
+              <img src="/images/pin.svg" />
+              <div>Taipei , Taiwan</div>
+            </div>
+            <div className="buttonBox">
             <div
               className="introductionButton"
               onClick={() => window.open('mailto:jack830608@gmail.com', '_blank')}
             >
               Contact Me
+          </div>
+          <div
+              className="introductionButton1"
+              onClick={() => document.getElementById('ofel_btn_container').click()}
+            >
+              Leave a message
+          </div>
           </div>
           </div>
           <div className={'introductionRight'}>
