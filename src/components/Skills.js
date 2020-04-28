5aa8e8e201223176a14e49b44ff9df61c17cecb7
JackLin <jack830608@gmail.com>
2020-04-28 11:53:44 +0800

    update skills css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,22 +3,49 @@ import SkillBar from 'react-skillbars';
 
 export default (props) => {
 
-  const skills = [
+  const FrontEnd = [
     { type: "HTML", level: 90 },
     { type: "CSS", level: 85 },
     { type: "SASS/SCSS", level: 70 },
     { type: "Javascript", level: 85 },
     { type: "React/Redux", level: 85 },
+    { type: "Next.js", level: 80 },
     { type: "Jquery", level: 75 },
-    { type: "Node.js", level: 80 },
-    { type: "MongoDB", level: 60 },
-    { type: "MySQL", level: 60 },
-    { type: "Docker", level: 70 },
-    { type: "ShellScript", level: 60 },
-    { type: "Python", level: 50 },
-    { type: "Git", level: 80 },
   ];
-  const colors = {
+  const BackEnd = [
+    { type: "Node.js", level: 80 },
+    { type: "Express.js", level: 85 },
+    { type: "RESTful API", level: 80 },
+    { type: "MongoDB", level: 65 },
+    { type: "MySQL", level: 65 },
+    { type: "Docker", level: 70 },
+    { type: "Python", level: 50 },
+  ];
+  const Others = [
+    { type: "AWS、GCP", level: 70 },
+    { type: "Nginx", level: 70 },
+    { type: "Gitflow", level: 85 },
+    { type: "Command Line", level: 70 },
+    { type: "ShellScript", level: 60 },
+    { type: "Jira", level: 80 },
+    { type: "Confluence", level: 80 },
+    { type: "GTM、GA", level: 75 },
+  ];
+  const FrontEndColors = {
+    "bar": "linear-gradient(45deg,rgba(75,105,205,0.4) ,rgba(135,153,220,0.8))",
+    "title": {
+      "text": "#ECF5FF",
+      "background": "linear-gradient(45deg,rgba(75,105,205,0.2) ,rgba(75,105,205,0.8))",
+    }
+  }
+  const BackEndColors = {
+    "bar": "linear-gradient(45deg,rgba(75,105,205,0.2) ,rgba(75,105,205,0.8))",
+    "title": {
+      "text": "#ECF5FF",
+      "background": "linear-gradient(45deg,rgba(75,105,205,0.4) ,rgba(135,153,220,0.8))",
+    }
+  }
+  const OthersColors = {
     "bar": "linear-gradient(45deg,rgba(75,105,205,0.4) ,rgba(135,153,220,0.8))",
     "title": {
       "text": "#ECF5FF",
@@ -35,12 +62,39 @@ export default (props) => {
         </h1>
         </div>
         <div className="skillsBox">
-          <SkillBar
-            skills={skills}
-            colors={colors}
-            animationDelay={0}
-            animationDuration={2000}
-          />
+          <div style={{ position: 'relative' }}>
+            <div className='skillsTitle'>
+              <div className="skillsSubtitle">Front-end</div>
+            </div>
+            <SkillBar
+              skills={FrontEnd}
+              colors={FrontEndColors}
+              animationDelay={0}
+              animationDuration={2000}
+            />
+          </div>
+          <div style={{ position: 'relative' ,margin:'15px 0 '}}>
+            <div className='skillsTitle'>
+              <div className="skillsSubtitle">Back-end</div>
+            </div>
+            <SkillBar
+              skills={BackEnd}
+              colors={BackEndColors}
+              animationDelay={0}
+              animationDuration={2000}
+            />
+          </div>
+          <div style={{ position: 'relative'}}>
+            <div className='skillsTitle'>
+              <div className="skillsSubtitle">Others</div>
+            </div>
+            <SkillBar
+              skills={Others}
+              colors={OthersColors}
+              animationDelay={0}
+              animationDuration={2000}
+            />
+          </div>
         </div>
       </div>
     </div>
