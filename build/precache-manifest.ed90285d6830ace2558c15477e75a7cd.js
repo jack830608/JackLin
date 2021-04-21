b95be31ad2ecfc69b5ec5d8140b91cb2e799b4c0
JackLin <jack.lin@positivegrid.com>
2021-04-21 10:10:06 +0800

    impl: add build

---
+++ build/precache-manifest.ed90285d6830ace2558c15477e75a7cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83ba3f191a1740d3a4e99d6968ce36c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67f5125e5a3e9a2b383d",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "ff5a9cf3e4a12799c609",
+    "url": "/static/css/main.c8545c39.chunk.css"
+  },
+  {
+    "revision": "67f5125e5a3e9a2b383d",
+    "url": "/static/js/2.b55dd33a.chunk.js"
+  },
+  {
+    "revision": "ff5a9cf3e4a12799c609",
+    "url": "/static/js/main.9c46f84d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
